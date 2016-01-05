5b715fc0db1b9ca7c4e25c9cc248f5b2feb34b08
cz-Michael <zchning@gmail.com>
2016-01-05 01:02:35 -0500

    add ng-match diretive to process password confirmation

--- www/js/controllers/signUpCtrl.js
+++ www/js/controllers/signUpCtrl.js
@@ -1,14 +1,18 @@
 'use strict';
 
-app.controller('SignUpCtrl', function($scope, $state) {
+app.controller('SignUpCtrl', function($scope, $state, $ionicLoading) {
     //Didsplay back button
     $scope.$on('$ionicView.beforeEnter', function (event, viewData) {
         viewData.enableBack = true;
     });
 
     $scope.signUp = function(user) {
+        $ionicLoading.show({
+          template: 'Loading...'
+        });
+        $ionicLoading.hide();
         console.log('Sign-In', user);
-        $state.go('tabs.home');
+        //$state.go('tabs.home');
     };
 
 })

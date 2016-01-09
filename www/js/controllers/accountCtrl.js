502221f2eca044408c0d79016e6f615f2f6d0dfe
cz-Michael <zchning@gmail.com>
2016-01-09 00:38:56 -0500

    add account page layout

--- www/js/controllers/accountCtrl.js
+++ www/js/controllers/accountCtrl.js
@@ -1,7 +1,33 @@
 'use strict';
 
-app.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+app.controller('AccountCtrl', function($scope, $ionicActionSheet, $cordovaCamera) {
+	$scope.settings = {
+		enableFriends: true
+	};
+
+	var hideSheet = null;
+	$scope.addImages = function() {
+		console.log("add images ...");
+		hideSheet = $ionicActionSheet.show({
+			buttons: [
+				{ text: '拍照' },
+				{ text: '从手机相册选择' }
+			],
+			//destructiveText: 'Delete',
+			titleText: '选择照片',
+			cancelText: '取消',
+			cancel: function() {
+				// add cancel code..
+			},
+			buttonClicked: function(index) {
+				console.log(index);
+				if (index === 0) {
+					openPhotoCamera();
+				} else {
+					openPhotoLibrary();
+				}
+				return true;
+			}
+		});
+	};
 });

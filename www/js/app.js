f0e118b2d7bce59e654849918fba943041f4c9f7
cz-Michael <zchning@gmail.com>
2016-01-10 00:11:17 -0500

    enable Done button in keyboard in ios

--- www/js/app.js
+++ www/js/app.js
@@ -6,7 +6,7 @@ app.run(function($ionicPlatform, $rootScope, $cordovaPush, $localStorage) {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
       cordova.plugins.Keyboard.disableScroll(true);
 
     }

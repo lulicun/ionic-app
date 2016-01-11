2dc484b6680d7f865d687c1eb0df354b72675bcd
cz-Michael <zchning@gmail.com>
2016-01-10 21:02:16 -0500

    only logged in user can post

--- www/js/controllers/momentCtrl.js
+++ www/js/controllers/momentCtrl.js
@@ -1,7 +1,30 @@
 'use strict';
 
-app.controller('MomentCtrl', function($scope, $ionicActionSheet, PostService) {
+app.controller('MomentCtrl', function($scope, $rootScope, $state, $ionicActionSheet, $ionicPopup, PostService) {
 	$scope.posts = PostService.all();
+
+	$scope.createPost = function() {
+		if ($rootScope.isLoggedIn) {
+			$state.go('tab.moment-create');
+		} else {
+			var myPopup = $ionicPopup.show({
+				template: '<p>只有登录后，老少爷们才知道是你发哩状态呀!</p>',
+				title: '登录后才能发到梁山圈',
+				subTitle: '登录请求',
+				scope: $scope,
+				buttons: [
+					{ text: '算啦！' },
+					{
+						text: '<b>去登录！</b>',
+						type: 'button-positive',
+						onTap: function(e) {
+							$state.go('signin');
+						}
+					}
+				]
+			});
+		}
+	}
 
 	$scope.addLike = function(post) {
 		console.log(post);
